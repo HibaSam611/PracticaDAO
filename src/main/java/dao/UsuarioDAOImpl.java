1c0d7be31b842fbed285e69e8b5a4be18854184e
Carmen <carmenjlr@gmail.com>
2025-11-16 21:03:21 +0100

    Commit de los cambios de CARMEN

---
+++ src/main/java/dao/UsuarioDAOImpl.java
@@ -0,0 +1,35 @@
+package dao;
+
+import model.Usuario;
+
+import java.util.List;
+
+public class UsuarioDAOImpl implements UsuarioDAO {
+
+    @Override
+    public void addUsuario(Usuario usuario) throws Exception {
+
+    }
+
+    @Override
+    public List<Usuario> getAllUsuarios() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Usuario getUsuarioById(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateUsuario(Usuario usuario) throws Exception {
+
+    }
+
+    @Override
+    public void deleteUsuario(int id) throws Exception {
+
+    }
+
+
+}

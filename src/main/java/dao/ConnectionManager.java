e7f882b2a047133eb1228c79822f5b01898ed444
Carmen <carmenjlr@gmail.com>
2025-11-21 22:09:15 +0100

    Commit de los cambios de CARMEN

--- src/main/java/dao/ConnectionManager.java
+++ src/main/java/dao/ConnectionManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
     private static final String URL = "jdbc:mysql://localhost:3306/Biblioteca?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "hola";
 
 
     static {
